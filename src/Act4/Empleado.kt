82c8f8cfe3a65976fa7cbb288ef7fd2e6adea57d
Albert Pla <alplsan@epsa.upv.es>
2019-10-28 22:50:49 +0100

    Act 4 do it okey

--- src/Act4/Empleado.kt
+++ src/Act4/Empleado.kt
@@ -2,9 +2,35 @@ package Act4
 
 class Empleado(var nombre: String, var sueldo: Double) {
 
-    fun imprimir() {
+    fun detalles() {
         println("$nombre tiene un sueldo de $sueldo")
     }
+}
+
+fun calcSueldos(empleados: MutableList<Empleado>) {
+    val sueldos = empleados.sumByDouble {
+        it.sueldo
+    }
+    println("Los gastos en sueldos de la empresa son :$sueldos")
+}
+
+fun main(args: Array<String>) {
+    val empleados: MutableList<Empleado> = mutableListOf(
+        Empleado("Pepe", 1000.0),
+        Empleado("Juan", 3500.0),
+        Empleado("Albert", 1500.0),
+        Empleado("Luis",2000.0))
+
+    empleados.forEach {
+        it.detalles()
+    }
+
+    calcSueldos(empleados)
+
+    empleados.add(Empleado("Pedro", 5000.0))
+
+    println("-------------------------------")
+    calcSueldos(empleados)
 }
 
 
