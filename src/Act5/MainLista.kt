0c51cc252b07aa9faf5dddbdea7269dca2d509c6
Albert Pla <alplsan@epsa.upv.es>
2019-10-28 23:13:32 +0100

    Act 5 do it

--- src/Act5/MainLista.kt
+++ src/Act5/MainLista.kt
@@ -1,2 +1,35 @@
 package Act5
+//Creacion con metodo
+fun dimealtura(): Float {
+    print("Dime una altura:")
+    val altura = readLine()!!.toFloat()
+    return altura
+}
+fun main(args: Array<String>) {
+  //Creacion lista sin metodo
+    /*var lista:MutableList<Double> = mutableListOf()
 
+    for (i in 0..4) {
+        print("Introduzca su altura $i :")
+        var nuevoValor: Double = readLine()!!.toDouble()
+        lista.add(nuevoValor)
+
+    }*/
+
+    val listaalturas: List<Float> = List(5) {
+        dimealtura()
+    }
+    val promedio = listaalturas.average()
+
+    println("La altura media de las personas introducidas es $promedio")
+
+    val mediaalto = listaalturas.count {
+        it > promedio
+    }
+    val mediabajo = listaalturas.count {
+        it < promedio
+    }
+
+    println("Hay  $mediaalto personas más altas que el  promedio ")
+    println("Hay $mediabajo personas mas bajas que el promedio ")
+}
